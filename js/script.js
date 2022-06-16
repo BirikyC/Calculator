25a0e042e51d680b4b7fc726988280677986d11f
BirikyC <adam.ceglarz18@gmail.com>
2022-06-16 03:24:52 +0200

    make a JavaScript folder and script file

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+	/*let num = 141
+	let num2 = '141'
+	let wynik = num === num2
+	console.log(wynik)*/
+	let b = "abbadabasdsa"
+	let a = "a"
+
+	let x = b.length
+	const y = x.Split(a)
+	console.log(x - y)
+})

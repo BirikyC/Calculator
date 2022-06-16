ab9694e5619369a4d9008a008a91f352c3bf1e12
BirikyC <adam.ceglarz18@gmail.com>
2022-06-16 16:12:22 +0200

    button display

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(function(){
-	
+	$('.btn').mousedown(function(){
+		$(this).css({right: '-.25em', bottom: '-.25em'})
+		$(this.children[0]).css({display: 'none'})
+		$(this.children[2]).css({display: 'none'})
+	}).on('mouseup mouseleave', function(){
+		$(this).css({right: 0, bottom: 0})
+		$(this.children[0]).css({display: 'initial'})
+		$(this.children[2]).css({display: 'initial'})
+	})
 })

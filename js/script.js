4ec92f0078936859d34f8a89bab6b5d02e3c46fe
BirikyC <adam.ceglarz18@gmail.com>
2022-06-17 04:03:29 +0200

    better button animation script

--- js/script.js
+++ js/script.js
@@ -18,12 +18,10 @@ $(function(){
 	})
 
 	$('.btn').mousedown(function(){
-		$(this).css({right: '-.25em', bottom: '-.25em'})
-		$(this.children[0]).css({display: 'none'})
-		$(this.children[2]).css({display: 'none'})
+		$($(this).children('span')).addClass('btn-clicked')
+		$($(this).children('div')).addClass('btn-clicked-children')
 	}).on('mouseup mouseleave', function(){
-		$(this).css({right: 0, bottom: 0})
-		$(this.children[0]).css({display: 'initial'})
-		$(this.children[2]).css({display: 'initial'})
+		$(this.children).removeClass('btn-clicked')
+		$(this.children).removeClass('btn-clicked-children')
 	})
 })
